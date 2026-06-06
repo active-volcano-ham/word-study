c2c00aa14f8828371b9e5e6de140101f4d4fd277
active-volcano-ham <hamham8218@gmail.com>
2026-06-06 11:57:36 +0900

    Add manifest and update index

---
+++ app/src/main/java/io/github/active_volcano_ham/twa/DelegationService.java
@@ -0,0 +1,14 @@
+package io.github.active_volcano_ham.twa;
+
+
+
+public class DelegationService extends
+        com.google.androidbrowserhelper.trusted.DelegationService {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        
+    }
+}
+
